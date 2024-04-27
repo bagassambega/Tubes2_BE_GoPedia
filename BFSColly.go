ebaa3e02b31a21a6319ec13edea4a7179c56f85e
b33rk <brkmanics@gmail.com>
2024-04-27 22:01:59 +0700

    save bfs

--- BFSColly.go
+++ BFSColly.go
@@ -190,3 +190,83 @@ func main() {
 	fmt.Println("Waktu eksekusi", end.Sub(startTime))
 	fmt.Println("Url visited: ", urlVisited)
 }
+// func BFS(start string, goal string, urlVisited *int) ([]string, bool) {
+// 	var shortestPath []string
+// 	// var allPath [][]string
+// 	var currLink string
+// 	var queue []string
+// 	var parent string
+// 	found := false
+// 	visited := make(map[string]bool)
+// 	history := make(map[string]string)
+// 	// var mutex sync.Mutex
+
+// 	// root := NewTreeNode(" ")
+// 	queue = append(queue, start)
+
+// 	c := colly.NewCollector()
+
+// 	c.OnRequest(func(r *colly.Request) {
+// 		// fmt.Println(r.URL)
+// 		*urlVisited++
+// 	})
+
+// 	fmt.Println("Start BFS")
+
+// 	c.OnHTML("div#mw-content-text a[href]", func(e *colly.HTMLElement) {
+// 		href := e.Attr("href")
+// 		if strings.HasPrefix(href, "/wiki/") && !checkIgnoredLink(href) {
+// 			kode := href[6:]
+// 			if href == "/wiki/"+goal {
+// 				found = true
+// 				history[kode] = currLink
+// 				e.Request.Abort()
+// 			} else {
+// 				queue = append(queue, kode)
+// 				if _,exists := history[kode]; !exists {
+// 					history[kode] = currLink
+// 				}
+// 				visited[kode] = false
+// 			}
+// 		}
+// 	})
+
+// 	c.OnError(func(r *colly.Response, err error) {
+// 		fmt.Println("Request URL:", r.Request.URL.String())
+// 		fmt.Println("Error:", err)
+// 	})
+// 	// limiter := make(chan int, 200)
+// 	for !found {
+// 		// mutex.Lock()
+// 		visited[parent] = true
+// 		// mutex.Unlock()
+
+// 		for _, element := range queue {
+// 			// limiter <- 1
+// 			// go func(link string) {
+// 			currLink = element
+// 			// mutex.Lock()
+// 			if !visited[currLink] {
+// 				// mutex.Unlock()
+// 				c.Visit("https://en.wikipedia.org/wiki/" + currLink)
+// 				queue = HapusAntrian(queue, &parent)
+// 			}
+// 			// <-limiter
+// 			// }(currLink)
+// 			if found {
+// 				break
+// 			}
+
+// 		}
+// 		queue = HapusAntrian(queue, &parent)
+// 	}
+
+// 	if found {
+// 		fmt.Println("Goal found")
+// 		shortestPath = getResult(history, goal, start)
+// 	} else {
+// 		fmt.Println("Goal not found")
+// 	}
+
+// 	return shortestPath, found
+// }
